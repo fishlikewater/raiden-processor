097560126aca4f22586964dc584f6aa7171a25e5
fish <fishlikewater@126.com>
2024-10-06 11:15:00 +0800

    feat: 删除不必要的code

--- src/main/java/io/github/fishlikewater/raiden/processor/AbstractRaidenProcessor.java
+++ src/main/java/io/github/fishlikewater/raiden/processor/AbstractRaidenProcessor.java
@@ -15,10 +15,16 @@
  */
 package io.github.fishlikewater.raiden.processor;
 
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.util.TreePath;
 import com.sun.source.util.Trees;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
+import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.ListBuffer;
+import com.sun.tools.javac.util.Name;
 import com.sun.tools.javac.util.Names;
 import io.github.fishlikewater.raiden.processor.context.RaidenContext;
 import lombok.Getter;
@@ -28,7 +34,7 @@ import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.util.Elements;
+import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.Types;
 
 /**
@@ -38,15 +44,10 @@ import javax.lang.model.util.Types;
  * @version 1.0.0
  * @since 2024/10/03
  */
+@Getter
 public abstract class AbstractRaidenProcessor extends AbstractProcessor {
 
-    @Getter
     private RaidenContext raidenContext;
-
-    public AbstractRaidenProcessor(RaidenContext raidenContext) {
-        super();
-        this.raidenContext = raidenContext;
-    }
 
     @Override
     public synchronized void init(ProcessingEnvironment env) {
@@ -55,18 +56,18 @@ public abstract class AbstractRaidenProcessor extends AbstractProcessor {
         Filer filer = processingEnv.getFiler();
         Messager messager = processingEnv.getMessager();
         Types types = processingEnv.getTypeUtils();
-        Elements elements = processingEnv.getElementUtils();
+        JavacProcessingEnvironment javaEvn = (JavacProcessingEnvironment) processingEnv;
 
-        Context context = ((JavacProcessingEnvironment) processingEnv).getContext();
+        Context context = javaEvn.getContext();
         Trees trees = Trees.instance(processingEnv);
         TreeMaker treeMaker = TreeMaker.instance(context);
         Names names = Names.instance(context);
 
-        raidenContext = RaidenContext.builder()
+        this.raidenContext = RaidenContext.builder()
                 .filer(filer)
                 .messager(messager)
                 .types(types)
-                .elements(elements)
+                .javaEvn(javaEvn)
                 .trees(trees)
                 .treeMaker(treeMaker)
                 .names(names)
@@ -76,5 +77,76 @@ public abstract class AbstractRaidenProcessor extends AbstractProcessor {
     @Override
     public SourceVersion getSupportedSourceVersion() {
         return SourceVersion.latestSupported();
+    }
+
+    public Name getNameFromString(String name) {
+        return this.raidenContext.getNames().fromString(name);
+    }
+
+    public JCTree.JCExpression literal(Object target) {
+        return this.raidenContext.getTreeMaker().Literal(target);
+    }
+
+    public JCTree.JCExpression assign(Name name, JCTree.JCExpression expr) {
+        return this.raidenContext.getTreeMaker().Assign(this.raidenContext.getTreeMaker().Ident(name), expr);
+    }
+
+    public JCTree.JCExpression memberAccess(String components) {
+        String[] componentArray = components.split("\\.");
+        JCTree.JCExpression expr = this.raidenContext.getTreeMaker().Ident(getNameFromString(componentArray[0]));
+        for (int i = 1; i < componentArray.length; i++) {
+            expr = this.raidenContext.getTreeMaker().Select(expr, getNameFromString(componentArray[i]));
+        }
+
+        return expr;
+    }
+
+    protected void variableDeclareClassAddImport(VariableElement variable, String fullQualifiedName) {
+        JCTree.JCClassDecl clazz = this.tryFindVariableDeclareClass(variable);
+        JCTree.JCCompilationUnit unit = (JCTree.JCCompilationUnit) this.raidenContext.getTrees().getPath(clazz.sym).getCompilationUnit();
+
+        ListBuffer<JCTree> imports = new ListBuffer<>();
+        for (int i = 0; i < unit.defs.size(); i++) {
+            imports.append(unit.defs.get(i));
+        }
+
+        JCTree.JCIdent packageIdent = this.raidenContext.getTreeMaker()
+                .Ident(this.getNameFromString(this.determineTargetPackageName(fullQualifiedName)));
+        JCTree.JCFieldAccess access = this.raidenContext.getTreeMaker()
+                .Select(packageIdent, this.raidenContext.getNames().fromString(this.determineTargetSimpleName(fullQualifiedName)));
+        JCTree.JCImport ipt = this.raidenContext.getTreeMaker().Import(access, false);
+        imports.append(ipt);
+
+        unit.defs = imports.toList();
+    }
+
+    protected JCTree.JCClassDecl tryFindVariableDeclareClass(VariableElement variable) {
+        TreePath treePath = this.raidenContext.getTrees().getPath(variable);
+
+        CompilationUnitTree ut = treePath.getCompilationUnit();
+
+        Tree tree = this.raidenContext.getTrees().getTree(variable);
+        if (tree instanceof JCTree.JCVariableDecl) {
+            JCTree.JCVariableDecl vd = (JCTree.JCVariableDecl) tree;
+
+            TreePath classTreePath = this.raidenContext.getTrees().getPath(ut, vd);
+            while (classTreePath != null) {
+                Tree classTree = classTreePath.getLeaf();
+                if (classTree instanceof JCTree.JCClassDecl) {
+                    return (JCTree.JCClassDecl) classTree;
+                }
+                classTreePath = classTreePath.getParentPath();
+            }
+        }
+
+        return null;
+    }
+
+    protected String determineTargetPackageName(String fullQualifiedName) {
+        return fullQualifiedName.substring(0, fullQualifiedName.lastIndexOf("."));
+    }
+
+    protected String determineTargetSimpleName(String fullQualifiedName) {
+        return fullQualifiedName.substring(fullQualifiedName.lastIndexOf(".") + 1);
     }
 }
--- src/main/java/io/github/fishlikewater/raiden/processor/context/RaidenContext.java
+++ src/main/java/io/github/fishlikewater/raiden/processor/context/RaidenContext.java
@@ -16,9 +16,8 @@
 package io.github.fishlikewater.raiden.processor.context;
 
 import com.sun.source.util.Trees;
-import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.TreeMaker;
-import com.sun.tools.javac.util.Name;
 import com.sun.tools.javac.util.Names;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,8 +26,6 @@ import lombok.NoArgsConstructor;
 
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
-import javax.lang.model.element.Element;
-import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 import java.io.Serializable;
 
@@ -48,73 +45,16 @@ public class RaidenContext implements Serializable {
     private static final long serialVersionUID = 5079936240211719665L;
 
     protected Filer filer;
+
     protected Messager messager;
+
     private Types types;
-    private Elements elements;
+
+    private JavacProcessingEnvironment javaEvn;
 
     private Trees trees;
 
     private TreeMaker treeMaker;
+
     private Names names;
-
-    // ----------------------------------------------------------------
-
-    public JCTree.JCVariableDecl toVariable(Element element) {
-        return (JCTree.JCVariableDecl) this.getTrees().getTree(element);
-    }
-
-    public Name getNameFromString(String name) {
-        return this.names.fromString(name);
-    }
-
-    public JCTree.JCExpression literal(Object target) {
-        return this.treeMaker.Literal(target);
-    }
-
-    public JCTree.JCExpression assign(Name name, JCTree.JCExpression expr) {
-        return this.getTreeMaker().Assign(this.getTreeMaker().Ident(name), expr);
-    }
-
-    public JCTree.JCAnnotation makeAnnotation(String annotationName, com.sun.tools.javac.util.List<JCTree.JCExpression> args) {
-        JCTree.JCExpression expression = chainDots(annotationName.split("\\."));
-        return this.getTreeMaker().Annotation(expression, args);
-    }
-
-    public JCTree.JCExpression memberAccess(String components) {
-        String[] componentArray = components.split("\\.");
-        JCTree.JCExpression expr = this.getTreeMaker().Ident(getNameFromString(componentArray[0]));
-        for (int i = 1; i < componentArray.length; i++) {
-            expr = treeMaker.Select(expr, getNameFromString(componentArray[i]));
-        }
-
-        return expr;
-    }
-
-    public JCTree.JCVariableDecl makeVarDef(
-            JCTree.JCModifiers modifiers,
-            String name,
-            JCTree.JCExpression varType,
-            JCTree.JCExpression init) {
-        return treeMaker.VarDef(
-                modifiers,
-                getNameFromString(name),
-                varType,
-                init
-        );
-    }
-
-    // ----------------------------------------------------------------
-
-    private JCTree.JCExpression chainDots(String... elems) {
-        assert elems != null;
-        JCTree.JCExpression e = null;
-        for (String elem : elems) {
-            e = e == null
-                    ? this.getTreeMaker().Ident(this.getNameFromString(elem))
-                    : this.getTreeMaker().Select(e, this.getNameFromString(elem));
-        }
-        assert e != null;
-
-        return e;
-    }
 }
